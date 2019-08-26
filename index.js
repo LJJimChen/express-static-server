2735c3cc60f760e21195519241fcbd925f2234f2
ubuntu <ubuntu@localhost.localdomain>
2019-08-26 16:42:04 +0800

    8088

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'));
 app.use(express.static('public'));
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.get('*', function (req, res) {
+    res.type('html');
+    let index = require('path').resolve("./public/index.html");
+    console.log(index);
+
+    res.sendFile(index);
+});
+var port = 8088;
+var host = "127.0.0.1"
+app.listen(port, host, (args) => console.log(`Example app listening on  ${host}:${port}!`))
